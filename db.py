c49a2f2cad1dd170ebe07a81278765e8b9a332d6
DONSIMON92 <semenische@yandex.com>
2020-06-04 07:20:23 +0000

    unfinished version

---
+++ db.py
@@ -0,0 +1,27 @@
+import sqlite3
+
+conn = sqlite3.connect('users.db')
+cursor = conn.cursor()
+
+def Create_database():
+    cursor.execute('CREATE TABLE IF NOT EXISTS Persons(user_id INTEGER PRIMARY KEY, name VARCHAR(35))')
+    conn.commit()
+
+def Join(user_id, name):
+    cursor.execute(f'INSERT OR IGNORE INTO Persons VALUES({user_id}, "{name}")')
+    print(f'user start bot|| name: {name}; id: {user_id}')
+    conn.commit()
+
+def Change(user_id, name):
+    print('change info about user')
+
+#def Search(user_id):
+#    if cursor.execute(f'SELECT * FROM Persons WHERE user_id = {user_id}') != None:
+#        print(cursor.execute(f'SELECT * FROM Persons WHERE user_id = {user_id}'))
+#        return True
+#    else:
+#        print(cursor.execute(f'SELECT * FROM Persons WHERE user_id = {user_id}'))
+#        return False
+
+def Ban():
+    print('banbanbanban')
---
+++ main.py
@@ -0,0 +1,73 @@
+from People import Person   #подключение модулей
+from db import Create_database, Join, Change, Ban
+from config import BOT_TOKEN
+import telegramcalendar
+
+from aiogram import Bot, types  #подключение библиотеки для работы с telegram
+from aiogram.bot import api
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils import executor
+
+bot = Bot(token = BOT_TOKEN)
+dp = Dispatcher(bot)
+
+Create_database()
+
+@dp.message_handler(commands=['start'])
+async def start_message(message: types.Message):
+    user_id = message.from_user.id  #считывание id и имени
+    if message.from_user.first_name != 'None':
+        name = message.from_user.first_name
+    elif message.from_user.username != 'None':
+        name = message.from_user.username
+    elif message.from_user.last_name != 'None':
+        name = message.from_user.last_name
+    else:
+        name = ''
+    Join(user_id, name)
+    await bot.send_sticker(message.chat.id, 'CAACAgIAAxkBAAIEqF5VL5ozeLnmwSaOJAbKQDQAAfidjQACYwkAAgk7OxMAAVFVxKRh8u0YBA')
+    await bot.send_message(message.chat.id, 'Этот бот нужен для создания заметок.\nОн может хранить ваши заметки и отправлять вам напоминания.\nДля работы с ним не нужна дополнительная авторизация(только аккуаунт в telegram), вы можете проверять записи и получать уведомления на другое устройство.\nЧтобы ознакомиться с функционалом нажмите на /commands')
+
+@dp.message_handler(commands=['choose'])
+async def choose(message: types.Message): 
+    await bot.send_message(message.chat.id, 'выберите день\n')
+
+@dp.message_handler(commands=['days'])
+async def days(message: types.Message): 
+    await bot.send_message(message.chat.id, 'выберите день\n')
+
+@dp.message_handler(commands=['today'])
+async def today(message: types.Message): 
+    await bot.send_message(message.chat.id, 'список день на сегодня\n\n')
+
+@dp.message_handler(commands=['thisweek'])
+async def thisweek(message: types.Message): 
+    await bot.send_message(message.chat.id, 'расписание на эту неделю: ')
+
+@dp.message_handler(commands=['settings'])
+async def settings(message: types.Message): 
+    await bot.send_message(message.chat.id, 'Вы можете изменить любую информацию о себе')
+
+@dp.message_handler(commands=['commands'])
+async def list_commands(message: types.Message):
+    await bot.send_message(message.chat.id,
+            'полный список команд, на которые отвечает бот\n\n'
+            '/days - просмотр записей на сегодня\n'
+            '/thisweek - просмотр  записей на неделю\n'
+            '/settings - настройки, которые вы можете посмотерть и изменить\n'
+            '/info - информация о боте\n'
+            '/contacts - информация о создателях и код программы\n')
+
+@dp.message_handler(commands=['info'])
+async def info(message: types.Message):
+    await bot.send_message(message.chat.id, '[О боте]\n Это приложение создано для планирования дел. Здесь вы можете создать своё расписание и ежедневно добавлять новые планы на день. В установленное время вам будет приходить уведомление.\nТакже бот залит на Github')
+
+@dp.message_handler(commands=['contacts'])
+async def contacts(message: types.Message):
+    keyboard = types.InlineKeyboardMarkup()
+    url_button = types.InlineKeyboardButton(text="Перейти на GitHub", url="https://github.com/DONSIMON92")
+    keyboard.add(url_button)
+    await bot.send_message(message.chat.id, 'Проект доступен на GitHub', reply_markup=keyboard)
+
+if __name__ == '__main__':  #зацикливание бота
+    executor.start_polling(dp)
